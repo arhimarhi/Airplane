126978924dd6f342ecc69236f3a41ad00c113cd1
arhimarhi <dmitrieva.kg@gmail.com>
2024-04-04 15:37:06 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Airplane o1 = new Airplane("TU-140", 450, 100);
+        Airplane o2 = new Airplane("AN-126", 550, 140);
+        System.out.println(o1);
+        System.out.println(o2);
+
+
+
+
+    }
+}
+class Airplane {
+    String name;
+    Double speed;
+    Double capacity;
+
+    public Airplane(String name, double speed, double capacity) {
+        this.name = name;
+
+    }
+
+    public Airplane(String name, Double speed, Double capacity) {
+        this.name = name;
+        this.speed = speed;
+        this.capacity = capacity;
+    }
+
+    public String toString() {
+        String s = "airplane" + name + "speed" + speed + "km/h" + "capacity " + capacity + "tons";
+        return s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double setSpeed(Double speed) {
+        this.speed = speed;
+        return 0;
+    }
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public double setCapacity(Double capacity) {
+        this.capacity = capacity;
+        return 0;
     }
 }
